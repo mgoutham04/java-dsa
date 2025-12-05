21c65a76cdcb27cbd6948dbdfb75b3c29a4f0fc2
mgoutham04 <kgoutham634@gmail.com>
2025-12-05 14:28:29 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+        while (true) { 
+            int n = sc.nextInt();
+            if(n%10==0){
+                continue;
+            }
+            System.out.println("number is "+n);
+        }
+    }
+        
+    }
