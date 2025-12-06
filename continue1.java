e346d4fda198909166081ec172af6f54200c4e17
mgoutham04 <kgoutham634@gmail.com>
2025-12-06 11:59:05 +0530

    Add files via upload

---
+++ continue1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class continue1{
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+        while (true) { 
+            int n = sc.nextInt();
+            if(n%10==0){
+                continue;
+            }
+            System.out.println("number is "+n);
+        }
+        
+    }
+        
+}
+
