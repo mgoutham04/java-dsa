dc6181fc02371ce111f9c8b5080d0d591a77357c
mgoutham04 <kgoutham634@gmail.com>
2025-12-06 11:59:24 +0530

    Add files via upload

---
+++ break1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class break1 {
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+        while (true) { 
+            int n = sc.nextInt();
+            if(n%10==0){
+                break;
+            }
+            System.out.println("number is "+n);
+        }
+    }
+        
+}
