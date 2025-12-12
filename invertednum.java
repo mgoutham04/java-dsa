fcba440c8345bebaedf077f364b5042ede871ba9
mgoutham04 <kgoutham634@gmail.com>
2025-12-12 16:11:40 +0530

    Add files via upload

---
+++ invertednum.java
@@ -0,0 +1,19 @@
+package basicsandpatterns;
+public class invertednum {
+    public static void inverted(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+                
+            }
+            
+            // for(int j=1;j<=n;j++){
+            //     System.out.print(" ");
+            // }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        inverted(5);
+    }
+}
